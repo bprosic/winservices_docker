72c4f4677bdea6746da4a6b128936f6f2b06c368
unknown <bprosic@itc203w11bp.pers.local>
2026-06-16 12:26:29 +0200

    D

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 docker exec -it winservices_docker node routes/createdb2.js
+
+create folders:
+mkdir -p /var/data/winservices/logs
+mkdir -p /var/data/winservices/upload
+then important!!
+
+sudo chown -R 1000:1000 ./var/data/winservices
+
+
+restart docker
+docker compose down -v
+docker compose up -d --build
--- app/config/dbcred.js
+++ app/config/dbcred.js
@@ -7,7 +7,7 @@ const MySQLStore = require("express-mysql-session")(session);
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PSW,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 20,
